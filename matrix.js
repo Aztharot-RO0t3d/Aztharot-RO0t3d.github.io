76eb72d0e6d8cd5c1d94490ca3eec8787f6a0ffb
Aztharot <130052303+Aztharot-RO0t3d@users.noreply.github.com>
2024-09-07 10:50:33 -0600

    Update matrix.js

--- matrix.js
+++ matrix.js
@@ -6,8 +6,8 @@ document.addEventListener('DOMContentLoaded', () => {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 
-    const matrix = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZabcdefghijklmnñopqrstuvwxyzёйцукенгшщзхъфывапролджэячсмитьбюЁЙЦУКЕНГШЩЗ-=ХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ123456789@#$%^&*()*&^%<>{}[]°¨`';
-    const fontSize = 14;
+    const matrix = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZabcdefghijklmnñopqrstuvwxyz123456789@#$%^&*()*&^%<>{}[]°¨`';
+    const fontSize = 12;
     const columns = canvas.width / fontSize;
     const drops = Array(Math.floor(columns)).fill(1);
 
